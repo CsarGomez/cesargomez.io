378a2d45807da7cb19c09857a591cec0f19228f0
Cesar Gomez <cgope1985@gmail.com>
2024-02-08 21:37:44 -0500

    fix experience item card styles

--- app/components/home/ExperienceListItem.tsx
+++ app/components/home/ExperienceListItem.tsx
@@ -23,7 +23,7 @@ const ExperienceListItem = ({ index, length, experience }: Props) => {
         index !== length - 1 && 'mb-10 '
       } ms-4 flex flex-col gap-1  font-sans`}
     >
-      <div className='absolute w-3 h-3 bg-sky-400 rounded-full mt-1.5 -start-1.5'></div>
+      <div className='absolute w-3 h-3 bg-sky-400 rounded-full -start-1.5'></div>
       <time className='mb-1 text-sm leading-none'>{experience.period}</time>
       <h6 className='font-bold border-none pb-0'>{experience.role}</h6>
       <div className='group flex items-center gap-2 text-sm text-neutral-400 dark:text-neutral-600 '>
