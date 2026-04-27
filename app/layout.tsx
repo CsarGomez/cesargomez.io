847a0b5e3075477a4a05b36a238d5873b0cfe43a
Cesar Gomez <cgope1985@gmail.com>
2026-04-27 10:28:29 -0500

    light and dark mode implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Bricolage_Grotesque, Inter, Lustria } from 'next/font/google';
 import '@/app/styles/globals.css';
 import { Header } from '@/components/header/Header';
 import { Footer } from '@/components/footer/Footer';
+import { Providers } from '@/app/providers/ThemeProvider';
 
 const bricolageGrotesque = Bricolage_Grotesque({
   variable: '--font-bricolage-grotesque-next',
@@ -33,12 +34,15 @@ export default function RootLayout({
   return (
     <html
       lang='en'
+      suppressHydrationWarning
       className={`${bricolageGrotesque.variable} ${inter.variable} ${lustria.variable} h-full antialiased`}
     >
       <body className='min-h-full flex flex-col justify-between bg-white text-black dark:bg-black dark:text-white font-inter'>
-        <Header />
-        {children}
-        <Footer />
+        <Providers>
+          <Header />
+          {children}
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
